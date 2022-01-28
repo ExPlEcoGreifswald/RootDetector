0020bc31dc2df071ebb44f480e3fd25be9659b31
Alexander Gillert <gillert@kingeider-u.ad.igd.fraunhofer.de>
2022-01-28 09:35:36 +0100

    fixes for building

--- build.py
+++ build.py
@@ -7,9 +7,9 @@ build_name = '%s_DigIT_RootDetector'%(datetime.datetime.now().strftime('%Y%m%d_%
 build_dir  = 'builds/%s'%build_name
 
 rc = subprocess.call(f'''pyinstaller --noupx                            \
-              --hidden-import=014a_tools     \
               --hidden-import=sklearn.utils._cython_blas     \
               --hidden-import=skimage.io._plugins.tifffile_plugin   \
+              --hidden-import=onnxruntime                           \
               --additional-hooks-dir=./hooks                        \
               --distpath {build_dir} main.py''')
 if rc!=0:
@@ -19,13 +19,10 @@ if rc!=0:
 
 shutil.copytree('HTML',   build_dir+'/HTML')
 shutil.copytree('models', build_dir+'/models')
-shutil.copytree('exclusionmask_models', build_dir+'/exclusionmask_models')
 if 'linux' in sys.platform:
     os.symlink('/main/main', build_dir+'/main.run')
 else:
     open(build_dir+'/main.bat', 'w').write(r'main\main.exe'+'\npause')
-    #takes up 400MB and is already in main/
-    os.remove(build_dir+'/main/tensorflow/python/_pywrap_tensorflow_internal.pyd')
 shutil.rmtree('./build')
 shutil.copyfile('settings.json', build_dir+'/settings.json')
 os.remove('./main.spec')
