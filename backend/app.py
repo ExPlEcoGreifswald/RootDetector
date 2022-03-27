e48160317e3e46d5e7275a03eb44d9f58592a65f
--- <--->
2022-03-27 14:16:21 +0200

    start of migration to DigIT_Base_UI

---
+++ backend/app.py
@@ -0,0 +1,32 @@
+from base.backend.app import App as BaseApp
+
+import os
+import flask
+
+import backend
+from . import root_detection
+
+
+
+class App(BaseApp):
+    def __init__(self, *args, **kw):
+        super().__init__(*args, **kw)
+        backend.init()
+        self.settings = backend
+
+
+    #override
+    def process_image(self, imagename):
+        print('Processing image:', imagename)
+        #FIXME: code duplication with upstream
+        full_path = os.path.join(self.cache_path, imagename)
+        if not os.path.exists(full_path):
+            flask.abort(404)
+        
+        result = root_detection.process_image(full_path)
+        result['segmentation'] = os.path.basename(result['segmentation'])
+        result['skeleton']     = os.path.basename(result['skeleton'])
+        return flask.jsonify(result)
+    
+
+
---
+++ frontend/root_settings.js
@@ -0,0 +1,47 @@
+
+
+//override
+function update_settings_modal(){
+    $('#settings-exclusionmask-enable').checkbox({onChange: on_exmask_checkbox});
+
+    const settings = GLOBAL.settings;
+    
+    var models_list = []
+    for(var modelname of settings.models)
+        models_list.push({name:modelname, value:modelname, selected:(modelname==settings.active_model)})
+    //TODO: if(settings.active_model=='')
+    //    models_list.push({name:'[UNSAVED MODEL]', value:'', selected:true})
+    $("#settings-active-model").dropdown({values: models_list, showOnFocus:false })
+
+
+    $('#settings-exclusionmask-enable').checkbox(settings.exmask_enabled? 'check' : 'uncheck');
+    var exmaskmodels_list = []
+    for(var name of settings.exmask_models)
+        exmaskmodels_list.push({name:name, value:name, selected:(name==settings.exmask_active_model)})
+    $("#settings-exclusionmask-model").dropdown({values: exmaskmodels_list, showOnFocus:false })
+
+    var trackingmodels_list = []
+    for(var name of settings.tracking_models)
+        trackingmodels_list.push({name:name, value:name, selected:(name==settings.tracking_active_model)})
+    $("#settings-tracking-model").dropdown({values: trackingmodels_list, showOnFocus:false })
+}
+
+
+//override
+function collect_settings_data(){
+    GLOBAL.settings.active_model          = $("#settings-active-model").dropdown('get value');
+    GLOBAL.settings.exmask_active_model   = $("#settings-exclusionmask-model").dropdown('get value');
+    GLOBAL.settings.tracking_active_model = $("#settings-tracking-model").dropdown('get value');
+    return {
+        active_model   : GLOBAL.settings.active_model,
+        exmask_enabled : GLOBAL.settings.exmask_enabled,
+        exmask_model   : GLOBAL.settings.exmask_active_model, 
+        tracking_model : GLOBAL.settings.tracking_active_model, 
+    }
+}
+
+function on_exmask_checkbox(){
+    var enabled = $('#settings-exclusionmask-enable').checkbox('is checked');
+    GLOBAL.settings.exmask_enabled = enabled;
+    $("#settings-exclusionmask-model-field").toggle(enabled)
+}
