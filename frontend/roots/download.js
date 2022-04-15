4334040d0f80e7423e8408dda7757b1b151ab5dc
alexander-g <3867427+alexander-g@users.noreply.github.com>
2022-04-15 09:38:14 +0200

    download all: folders+combined stats

--- frontend/roots/download.js
+++ frontend/roots/download.js
@@ -1,8 +1,6 @@
 
 
-
-
-RootDetectionDownload = class RootDetectionDownload extends BaseDownload{
+RootDetectionDownload = class extends BaseDownload{
     //override
     static zipdata_for_file(filename){
         var f                           = GLOBAL.files[filename];
@@ -18,13 +16,30 @@ RootDetectionDownload = class RootDetectionDownload extends BaseDownload{
         return zipdata;
     }
 
-    static csv_data_for_file(filename){
-        var csvtxt = 'Filename, '
+    //override
+    static zipdata_for_files(filenames){
+        var zipdata      = super.zipdata_for_files(filenames)
+        var combined_csv = ''
+        for(var i in filenames){
+            var single_csv = this.csv_data_for_file(filenames[i], i==0)
+            if(single_csv!=undefined)
+                combined_csv += single_csv;
+        }
+        if(combined_csv.length > 0)
+            zipdata['statistics.csv'] = combined_csv;
+        return zipdata;
+    }
+
+    static csv_data_for_file(filename, header=true){
+        var csvtxt = '';
+        if(header){
+            csvtxt += 'Filename, '
                 + '# root pixels, # background pixels, '
                 + '# mask pixels, # skeleton pixels, '
                 + '# skeleton pixels (<3px width), # skeleton pixels (3-7px width), # skeleton pixels (>7px width),'
                 + 'Kimura length'
                 + ';\n';
+        }
         
         var f = GLOBAL.files[filename]
         if(!f.results)
@@ -78,27 +93,37 @@ RootTrackingDownload = class extends BaseDownload {
 
     //override
     static on_download_all(event){
-        var zipdata   = {}
-        var filenames = Object.keys(GLOBAL.files)
-        //testing all possible combinations. FIXME: make this a bit smarter
+        var filenames          = Object.keys(GLOBAL.files)
+        var filenames_combined = []
+        var filenames_pairs    = []
         for(var filename0 of filenames){
-            for(var filename1 of filenames){
-                var combined = `${filename0}.${filename1}`
-                var fzipdata = this.zipdata_for_file(combined)
-                if(fzipdata == undefined)
-                    continue;
-            
-                Object.assign(zipdata, fzipdata)
-            }
+            var tracking_results = GLOBAL.files[filename0].tracking_results
+            if(tracking_results == undefined)
+                continue;
+            for(var filename1 of Object.keys(tracking_results))
+                if(Object.keys(tracking_results[filename1]).length>0){
+                    filenames_combined.push(`${filename0}.${filename1}`)
+                    filenames_pairs.push([filename0, filename1])
+                }
         }
+
+        var zipdata = this.zipdata_for_files(filenames_combined)
+        var combined_csv = ''
+        for(var i in filenames_combined){
+            var single_csv = this.csv_data_statistics(...filenames_pairs[i], i==0)
+            if(single_csv!=undefined)
+                combined_csv += single_csv;
+        }
+        if(combined_csv.length > 0)
+            zipdata['statistics.csv'] = combined_csv;
 
         if(Object.keys(zipdata).length==0)
             return
-        download_zip('results.zip', zipdata)
+        download_zip('tracking_results.zip', zipdata)
     }
 
 
-    static csv_data_statistics(filename0, filename1){
+    static csv_data_statistics(filename0, filename1, include_header=true){
         var stats = GLOBAL.files[filename0].tracking_results[filename1].statistics;
 
         var header = [
@@ -119,6 +144,6 @@ RootTrackingDownload = class extends BaseDownload {
             return;
         }
 
-        return [header.join(', '), data.join(', '), ''].join(';\n')
+        return (include_header? [header] : []).concat([data.join(', '), '']).join(';\n')
     }
 }
