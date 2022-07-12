8b4e8ddde798f1c7d758cf762a6f4d87adcb3b13
alexander-g <3867427+alexander-g@users.noreply.github.com>
2022-07-12 14:58:18 +0200

    segmentation model src

---
+++ models_src/2022-07-11_029/models.py
@@ -0,0 +1,186 @@
+import sys, time, os, io, warnings, importlib
+import typing as tp
+import numpy as np
+import torch, torchvision
+from torchvision.models._utils import IntermediateLayerGetter
+import PIL.Image
+
+warnings.simplefilter('ignore') #pytorch is too noisy
+
+
+if "__torch_package__" in dir():
+    #inside a torch package
+    import torch_package_importer
+    import_func = torch_package_importer.import_module 
+else:
+    #normal
+    import importlib
+    import_func = lambda m, p: importlib.reload(importlib.import_module(m, p))
+
+#internal modules
+MODULES = ['datasets', 'training']
+[datasets, traininglib] = [import_func('.'+m, __package__) for m in MODULES]
+
+
+
+
+class UNet(torch.nn.Module):
+    class UpBlock(torch.nn.Module):
+        def __init__(self, in_c, out_c, inter_c=None):
+            #super().__init__()
+            torch.nn.Module.__init__(self)
+            inter_c        = inter_c or out_c
+            self.conv1x1   = torch.nn.Conv2d(in_c, inter_c, 1)
+            self.convblock = torch.nn.Sequential(
+                torch.nn.Conv2d(inter_c, out_c, 3, padding=1, bias=0),
+                torch.nn.BatchNorm2d(out_c),
+                #torch.nn.ReLU(),
+            )
+        def forward(self, x, skip_x, relu=True):
+            x = torch.nn.functional.interpolate(x, skip_x.shape[2:])
+            x = torch.cat([x, skip_x], dim=1)
+            x = self.conv1x1(x)
+            x = self.convblock(x)
+            x = torch.relu(x) if relu else x
+            return x
+    
+    def __init__(self, backbone='mobilenet3l', pretrained:bool=True):
+        torch.nn.Module.__init__(self)
+        factory_func = globals().get(f'{backbone}_backbone', None)
+        if factory_func is None:
+            raise NotImplementedError(backbone)
+        self.backbone, C = factory_func(pretrained)
+        
+        self.up0 = self.UpBlock(C[-1]    + C[-2],  C[-2])
+        self.up1 = self.UpBlock(C[-2]    + C[-3],  C[-3])
+        self.up2 = self.UpBlock(C[-3]    + C[-4],  C[-4])
+        self.up3 = self.UpBlock(C[-4]    + C[-5],  C[-5])
+        self.up4 = self.UpBlock(C[-5]    + 3,      32)
+        self.cls = torch.nn.Conv2d(32, 1, 3, padding=1)
+    
+    def forward(self, x, return_features=False, sigmoid=True):
+        device = list(self.parameters())[0].device
+        x      = x.to(device)
+        
+        X = self.backbone(x)
+        X = ([x] + [X[f'out{i}'] for i in range(5)])[::-1]
+        x = X.pop(0)
+        x = self.up0(x, X[0])
+        x = self.up1(x, X[1])
+        x = self.up2(x, X[2])
+        x = self.up3(x, X[3])
+        x = self.up4(x, X[4], relu=not return_features)
+        if return_features:
+            return normalize(x)
+        x = self.cls(x)
+        if sigmoid:
+            x = torch.sigmoid(x)
+        return x
+    
+    def load_image(self, path):
+        return PIL.Image.open(path) / np.float32(255)
+    
+    def process_image(self, image, use_onnx=True, progress_callback=lambda x:None, batchsize=4, threshold=0.5):
+        #TODO? slice into patches
+        if isinstance(image, str):
+            image = self.load_image(image)
+        
+        x = torchvision.transforms.ToTensor()(image)[None]
+        with torch.no_grad():
+            y = self(x).cpu()
+        output = y.numpy().squeeze()
+        if threshold is not None:
+            output = (output > threshold)*1
+        return output
+
+    def start_training(self, 
+                       imagefiles_train,      targetfiles_train, 
+                       imagefiles_valid=None, targetfiles_valid=None, 
+                       epochs=100, callback=None, num_workers='auto',
+                       ds_kwargs={}, task_kwargs={}):
+        task     = traininglib.SegmentationTask(self, epochs=epochs, callback=callback, **task_kwargs)
+        ds_train = datasets.Dataset(imagefiles_train, targetfiles_train, augment=True, **ds_kwargs)
+        ld_train = ds_train.create_dataloader(batch_size=8, shuffle=True, num_workers=num_workers)
+        ld_valid = None
+        if imagefiles_valid is not None and targetfiles_valid is not None:
+            ds_valid = datasets.Dataset(imagefiles_valid, targetfiles_valid, augment=False, patchsize=5000, **ds_kwargs)  #full sized images
+            ld_valid = ds_valid.create_dataloader(batch_size=1, shuffle=False, num_workers=num_workers)
+        self.requires_grad_(True)
+        task.fit(ld_train, ld_valid, epochs)
+        self.eval().cpu().requires_grad_(False)
+
+    def stop_training(self):
+        traininglib.TrainingTask.request_stop()
+
+    def save(self, destination):
+        if isinstance(destination, str):
+            destination = time.strftime(destination)
+            if not destination.endswith('.pt.zip'):
+                destination += '.pt.zip'
+        try:
+            import torch_package_importer as imp
+            #re-export
+            importer = (imp, torch.package.sys_importer)
+        except ImportError as e:
+            #first export
+            importer = (torch.package.sys_importer,)
+        with torch.package.PackageExporter(destination, importer) as pe:
+            interns = [__name__.split('.')[-1]]+MODULES
+            pe.intern(interns)
+            pe.extern('**', exclude=['torchvision.**'])
+            externs = ['torchvision.ops.**', 'torchvision.datasets.**', 'torchvision.io.**', 'torchvision.models.*']
+            pe.intern('torchvision.**', exclude=externs)
+            pe.extern(externs)
+            
+            #force inclusion of internal modules + re-save if importlib.reload'ed
+            for inmod in interns:
+                inmod_pkg = f'{__package__}.{inmod}' if __package__ else inmod
+                if inmod_pkg in sys.modules:
+                    pe.save_source_file(inmod, sys.modules[inmod_pkg].__file__, dependencies=True)
+                else:
+                    pe.save_source_string(inmod, importer[0].get_source(inmod))
+            
+            pe.save_pickle('model', 'model.pkl', self)
+        return destination
+
+
+def normalize(x, axis=-3):
+    denom = (x**2).sum(axis, keepdims=True)**0.5
+    return x / denom
+
+
+def resnet18_backbone(pretrained:bool):
+    base = torchvision.models.resnet18(pretrained=pretrained)
+    return_layers = dict(relu='out0', layer1='out1', layer2='out2', layer3='out3', layer4='out4')
+    backbone = IntermediateLayerGetter(base, return_layers)
+    channels = [64, 64, 128, 256, 512]
+    return backbone, channels
+
+def mobilenet2_backbone(pretrained:bool):
+    base = torchvision.models.mobilenet_v2(pretrained=pretrained).features
+    return_layers = {'1':'out0', '3':'out1', '6':'out2', '13':'out3', '18':'out4'}
+    backbone = IntermediateLayerGetter(base, return_layers)
+    channels = [16, 24, 32, 96, 1280]
+    return backbone, channels
+
+def mobilenet3s_backbone(pretrained:bool):
+    base = torchvision.models.mobilenet_v3_small(pretrained=pretrained).features
+    return_layers = {'0':'out0', '1':'out1', '3':'out2', '8':'out3', '12':'out4'}
+    backbone = IntermediateLayerGetter(base, return_layers)
+    channels = [16, 16, 24, 48, 576]
+    return backbone, channels
+
+def mobilenet3s_mini_backbone(pretrained:bool):
+    base = torchvision.models.mobilenet_v3_small(pretrained=pretrained).features
+    return_layers = {'0':'out0', '1':'out1', '3':'out2', '8':'out3', '9':'out4'}
+    backbone = IntermediateLayerGetter(base, return_layers)
+    channels = [16, 16, 24, 48, 96]
+    return backbone, channels
+
+def mobilenet3l_backbone(pretrained:bool):
+    base = torchvision.models.mobilenet_v3_large(pretrained=pretrained).features
+    return_layers = {'1':'out0', '3':'out1', '6':'out2', '10':'out3', '16':'out4'}
+    backbone = IntermediateLayerGetter(base, return_layers)
+    channels = [16, 24, 40, 80, 960]
+    return backbone, channels
+
---
+++ models_src/2022-07-11_029/training.py
@@ -0,0 +1,238 @@
+import numpy as np
+import torch, torchvision
+
+
+
+
+class TrainingTask(torch.nn.Module):
+    def __init__(self, basemodule, epochs=10, lr=0.001, optim='AdamW', callback=None):
+        super().__init__()
+        self.basemodule        = basemodule
+        self.epochs            = epochs
+        self.lr                = lr
+        self.progress_callback = callback
+        assert optim in ['SGD', 'Adam', 'AdamW']
+        self.optim             = optim
+    
+    def training_step(self, batch):
+        raise NotImplementedError()
+    def validation_step(self, batch):
+        raise NotImplementedError()
+    def validation_epoch_end(self, logs):
+        raise NotImplementedError()
+    
+    def configure_optimizers(self):
+        if self.optim=='SGD':
+            optim = torch.optim.SGD(self.parameters(), self.lr, momentum=0.9, weight_decay=1e-4)
+        elif self.optim=='Adam':
+            optim = torch.optim.Adam(self.parameters(), self.lr, weight_decay=1e-4)
+        elif self.optim=='AdamW':
+            optim = torch.optim.AdamW(self.parameters(), self.lr, weight_decay=0.01)
+        #steps = [int(self.epochs*i) for i in [0.6,0.8,0.92]]
+        #print('Learning rate milestones:', steps)
+        #sched = torch.optim.lr_scheduler.MultiStepLR(optim, steps, gamma=0.2)
+        sched = torch.optim.lr_scheduler.CosineAnnealingLR(optim, self.epochs, eta_min=self.lr*0.01)
+        return optim, sched
+    
+    @property
+    def device(self):
+        return next(self.parameters()).device
+    
+    def train_one_epoch(self, loader, optimizer, scheduler=None):
+        for i,batch in enumerate(loader):
+            if self.__class__.stop_requested:
+                break
+            loss,logs  = self.training_step(batch)
+            
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            logs['lr'] = optimizer.param_groups[0]['lr']
+            self.callback.on_batch_end(logs, i, len(loader))
+        if scheduler:
+            scheduler.step()
+    
+    def eval_one_epoch(self, loader):
+        all_outputs = []
+        for i,batch in enumerate(loader):
+            outputs, logs  = self.validation_step(batch)
+            self.callback.on_batch_end(logs, i, len(loader))
+            all_outputs   += [outputs]
+        logs = self.validation_epoch_end(all_outputs)
+        self.callback.on_batch_end(logs, i, len(loader))
+    
+    def fit(self, loader_train, loader_valid=None, epochs='auto'):
+        self.epochs = epochs
+        if epochs == 'auto':
+            self.epochs = max(15, 50 // len(loader_train))
+            
+        if self.progress_callback is not None:
+            self.callback = TrainingProgressCallback(self.progress_callback, self.epochs)
+        else:
+            self.callback = PrintMetricsCallback()
+        
+        self.train().requires_grad_(True)
+        optim, sched  = self.configure_optimizers()
+        device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        torch.cuda.empty_cache()
+        try:
+            self.to(device)
+            self.__class__.stop_requested = False
+            for e in range(self.epochs):
+                if self.__class__.stop_requested:
+                    break
+                self.train().requires_grad_(True)
+                self.train_one_epoch(loader_train, optim, sched)
+                
+                self.eval().requires_grad_(False)
+                if loader_valid:
+                    self.eval_one_epoch(loader_valid)
+                
+                self.callback.on_epoch_end(e)
+        except KeyboardInterrupt:
+            print('\nInterrupted')
+        except Exception as e:
+            #prevent the exception getting to ipython (memory leak)
+            import traceback
+            traceback.print_exc()
+            return e
+        finally:
+            self.zero_grad(set_to_none=True)
+            self.eval().cpu().requires_grad_(False)
+            torch.cuda.empty_cache()
+     
+    #XXX: class method to avoid boiler code
+    @classmethod
+    def request_stop(cls):
+        cls.stop_requested = True
+
+
+class SegmentationTask(TrainingTask):
+    def __init__(self, *args, loss='wfocal', loss_kw={}, **kw):
+        super().__init__(*args, **kw)
+        assert loss  in ['BCE', 'wBCE', 'wBCE+Dice', 'focal', 'wfocal']
+        self.loss      = loss
+        if 'focal' in loss and loss_kw=={}:
+            loss_kw    = {'alpha':0.7, 'gamma':2.0}  #higher alpha to put focus on the positive class
+        self.loss_kw   = loss_kw
+
+    def training_step(self, batch):
+        x,y     = batch
+        x,y     = x.to(self.device), y.to(self.device)
+        sigmoid = self.loss not in ['focal', 'wfocal']
+        y_seg   = self.basemodule(x, sigmoid=sigmoid)
+        if self.loss=='BCE':
+            loss0   = torch.nn.functional.binary_cross_entropy(y_seg, y).mean()
+        elif self.loss=='wBCE':
+            loss0   = weighted_bce_loss(y_seg, y).mean()
+        elif self.loss=='wBCE+Dice':
+            loss0   = weighted_dice_entropy_loss(y_seg, y).mean()
+        elif self.loss=='focal':
+            loss0   = torchvision.ops.sigmoid_focal_loss( y_seg, y,  **self.loss_kw).mean()
+        elif self.loss=='wfocal':
+            loss0   = weighted_focal_loss(y_seg, y,  **self.loss_kw).mean()
+        return loss0, {'loss': loss0.item()}
+    
+    def validation_step(self, batch):
+        x,y     = batch
+        x,y     = x.to(self.device), y.to(self.device)
+        ypred   = self.basemodule(x)
+        dice    = dice_score(ypred, y).mean()
+        ypred   = (ypred>0.5).float()
+        TP      = (ypred *     y).sum([1,2,3]).cpu().numpy()
+        FP      = (ypred * (1-y)).sum([1,2,3]).cpu().numpy()
+        FN      = ((1-ypred) * y).sum([1,2,3]).cpu().numpy()
+        return {
+            'precision' : TP/(TP+FP),
+            'recall'    : TP/(TP+FN),
+            'IoU'       : IoU(y, ypred).cpu().numpy(),
+             }, None
+    
+    def validation_epoch_end(self, outs):
+        precision = np.concatenate([o['precision'] for o in outs])
+        recall    = np.concatenate([o['recall']    for o in outs])
+        iou       = np.concatenate([o['IoU']       for o in outs])
+        return {'precision': np.nanmean(precision), 'recall':np.nanmean(recall), 'IoU':np.nanmean(iou)}
+
+
+def dice_score(ypred, ytrue, eps=1):
+    '''Per-image dice score'''
+    d = torch.sum(ytrue, dim=[2,3]) + torch.sum(ypred, dim=[2,3]) + eps
+    n = 2* torch.sum(ytrue * ypred, dim=[2,3] ) +eps
+    return torch.mean(n/d, dim=1)
+
+def dice_loss(ypred, ytrue):
+    return 1-dice_score(ypred,ytrue)
+
+def dice_entropy_loss(ypred, ytrue, alpha=0.01, with_logits=False):
+    bce_func = torch.nn.functional.binary_cross_entropy_with_logits if with_logits else torch.nn.functional.binary_cross_entropy
+    return (  dice_loss(ypred, ytrue)[:,np.newaxis, np.newaxis]*alpha 
+            + bce_func(ypred, ytrue, reduction='none')*(1-alpha) ).mean()
+
+def weightfunc(x, alpha=2, beta=0.5):
+    x = torch.nn.functional.avg_pool2d(x, kernel_size=(5,5), stride=1, padding=2)
+    return 1 + 10 * torch.exp( -(  x - 0.5  )**2 / 0.05 )
+
+def weightfunc(x, alpha=2, beta=0.5):
+    x = torch.nn.functional.avg_pool2d(x, kernel_size=(5,5), stride=1, padding=2)
+    return 1 - torch.tanh( (x-beta)*alpha )**2
+
+def weighted_loss(baseloss):
+    def lossfunction(ypred, ytrue, *args, **kwargs):
+        return (baseloss(ypred, ytrue, *args, **kwargs) * weightfunc(ytrue)).mean()
+    lossfunction.__name__=baseloss.__name__
+    return lossfunction
+
+weighted_dice_entropy_loss    = weighted_loss(dice_entropy_loss)
+weighted_bce_loss             = weighted_loss(lambda *x: torch.nn.functional.binary_cross_entropy(*x, reduction='none'))
+weighted_bce_loss_with_logits = weighted_loss(lambda *x: torch.nn.functional.binary_cross_entropy_with_logits(*x, reduction='none'))
+weighted_focal_loss           = weighted_loss(torchvision.ops.sigmoid_focal_loss)
+
+
+def IoU(a,b):
+    a,b = torch.as_tensor(a).bool(), torch.as_tensor(b).bool()
+    #single channel inputs required
+    assert a.ndim==2 or a.shape[-3]==1
+    intersection = a & b
+    union        = a | b
+    return (intersection.sum(dim=[-1,-2]) / union.sum(dim=[-1,-2]))
+
+
+
+
+class PrintMetricsCallback:
+    '''Prints metrics after each training epoch in a compact table'''
+    def __init__(self):
+        self.epoch = 0
+        self.logs  = {}
+        
+    def on_epoch_end(self, epoch):
+        self.epoch = epoch + 1
+        self.logs  = {}
+        print() #newline
+    
+    def on_batch_end(self, logs, batch_i, n_batches):
+        if logs is not None:
+            self.accumulate_logs(logs)
+        percent     = ((batch_i+1) / n_batches)
+        metrics_str = ' | '.join([f'{k}:{float(np.mean(v)):>9.5f}' for k,v in self.logs.items()])
+        print(f'[{self.epoch:04d}|{percent:.2f}] {metrics_str}', end='\r')
+    
+    def accumulate_logs(self, newlogs):
+        for k,v in newlogs.items():
+            self.logs[k] = self.logs.get(k, []) + [v]
+
+class TrainingProgressCallback:
+    '''Passes training progress as percentage to a custom callback function'''
+    def __init__(self, callback_fn, epochs):
+        self.n_epochs    = epochs
+        self.epoch       = 0
+        self.callback_fn = callback_fn
+    
+    def on_batch_end(self, logs, batch_i, n_batches):
+        percent     = ((batch_i+1) / (n_batches*self.n_epochs))
+        percent    += self.epoch / self.n_epochs
+        self.callback_fn(percent)
+    
+    def on_epoch_end(self, epoch):
+        self.epoch = epoch + 1
