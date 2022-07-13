494c5aa2f7fd5136c580d6b7d83ea61dc33677e1
alexander-g <3867427+alexander-g@users.noreply.github.com>
2022-07-13 13:37:06 +0200

    number of training files infobox

--- frontend/roots/training.js
+++ frontend/roots/training.js
@@ -1,6 +1,12 @@
 
 
 RootsTraining = class extends BaseTraining {
+
+    //override
+    static refresh_tab(){
+        super.refresh_tab()
+        this.update_number_of_training_files_info()
+    }
     
     //dummy override: all files selected  //TODO: move upstream
     static get_selected_files(){
@@ -25,6 +31,12 @@ RootsTraining = class extends BaseTraining {
             return;
         
         super.update_model_info(model_type)
+    }
+
+    static update_number_of_training_files_info(){
+        const n = this.get_selected_files().length;
+        $('#training-number-of-files-info-label').text(n)
+        $('#training-number-of-files-info-message').removeClass('hidden')
     }
 }
 
